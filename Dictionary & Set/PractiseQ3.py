88ea9f2808c9d2b18ee4d12021a72a21fa646f49
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-01-28 10:33:55 +0530

    Practise Que3

---
+++ Dictionary & Set/PractiseQ3.py
@@ -0,0 +1,8 @@
+Marks = {}
+x1 = int(input("Enter Phy   : "))
+Marks.update({"Phy"   : x1})
+x2 = int(input("Enter Chem  : "))
+Marks.update({"Chem"  : x2})
+x3 = int(input("Enter Maths : "))
+Marks.update({"Maths" : x3})
+print(Marks)

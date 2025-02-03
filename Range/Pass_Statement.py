25a01f248a241296ceba5dc0d0bc934ad27e4517
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-03 08:55:29 +0530

    Rnage

---
+++ Range/Pass_Statement.py
@@ -0,0 +1,4 @@
+for el in range(10):
+    pass
+
+print("Pass is a null statement that does nothing. It is used as a placeholder for future code.")
---
+++ Range/Practise.py
@@ -0,0 +1,13 @@
+# Print numbers from 1 to 100
+for i in range(1, 101):
+    print(i)
+print("------------")
+
+# Print numbers from 100 to 1.
+for i in range(100, 0, -1):
+    print(i)
+print("------------")
+
+n = int(input("Enter a number : "))
+for i in range(1, 11):
+    print(n*i)

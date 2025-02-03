a86643f42e6174fd35bf1358c7146920fa8f9579
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-03 08:09:08 +0530

    Range

---
+++ Range/range.py
@@ -0,0 +1,18 @@
+# seq = range(10)
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+
+for i in range(10): #range(stop)
+    print(i)
+print("-------------")
+
+for i in range(2, 10):  #range(start, stop)
+    print(i)
+print("-------------")
+
+for i in range(2, 20, 2): #range(start, stop, step)
+    print(i)
+print("-------------")

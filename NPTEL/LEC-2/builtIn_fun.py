e27b197261a8d58a641070f5ab5b6dc461e273b2
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-10-14 10:57:16 +0530

    built in functions

---
+++ NPTEL/LEC-2/builtIn_fun.py
@@ -0,0 +1,8 @@
+# print(type(input))
+# a = input('') #not a mathematical funtion, always return a string
+# print(a)
+
+temp_in_celsius = float(input('Celsius: '))
+fahrenhiet = 1.8 * temp_in_celsius + 32
+
+print(fahrenhiet)

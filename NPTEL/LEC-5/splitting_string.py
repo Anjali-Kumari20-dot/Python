96ecc054d7c043e0872ac6653ca05cb4ce11c0df
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-10-25 20:30:10 +0530

    Exception handling

--- NPTEL/LEC-5/splitting_string.py
+++ NPTEL/LEC-5/splitting_string.py
@@ -1,16 +1,21 @@
 date = '25/10/2025'
 print(type(date) ) # str
-# day, month, year = date.split('/')
-# print(day)    # '27'    
-# print(month)  # '02'
-# print(year)   # '2025'
+day, month, year = date.split('/')
+try:
+    day = int(day)
+    month = int(month)
+    year = int(year)
+except:
+    raise ValueError(f'Illegal date format: {date}')
 
-date = date.split('/')  # ['25', '10', '2025']
-print(date[0])  # '25'
+# date = date.split('/')  # ['25', '10', '2025']
+# print(date[0])  # '25'
 
-print(id(date)) # list object id
-print(id(date[0])) # str object id '25'
+# print(id(date)) # list object id
+# print(id(date[0])) # str object id '25'
 
-date[0] = '26' # mutating the list: replacing '25' with '26'
-print(id(date[0])) # new str object id '26'
-print(id(date)) # list object id remains the same
+# date[0] = '26' # mutating the list: replacing '25' with '26'
+# print(id(date[0])) # new str object id '26'
+# print(id(date)) # list object id remains the same
+
+
